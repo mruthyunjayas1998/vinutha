527e5d4a0f6997533c9f1c13f651311d7d092665
mruthyunjayas1998 <85325215+mruthyunjayas1998@users.noreply.github.com>
2022-01-22 00:20:06 +0530

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Baby..", "Paru..", "Vinu..", "Bangari.."];
+const textArray = ["Baby..", "Vinu..", "Bangari..", "BhrammaRakshasi"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
